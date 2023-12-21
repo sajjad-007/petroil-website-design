f549165f7dccb046e727620ecadf7a29c33620d6
sajjad-007 <sajjadhossain8123@gmail.com>
2023-12-21 15:32:54 +0600

    header section complete

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          headingbg: '#282828',
+        },
+        spacing: {
+            'container': '1144px',
+        },
+        fontFamily: {
+            'poppins': "'Poppins', sans-serif",
+          }
+      }
+    }
+  }
