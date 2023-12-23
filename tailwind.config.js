66379df882d9a1eda12a10f0be68164fed899f91
sajjad-007 <sajjadhossain8123@gmail.com>
2023-12-23 20:20:30 +0600

    blog section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,10 @@ tailwind.config = {
           'serviceimg2': "linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6)) ,url('./images/service_img2.png')",
           'serviceimg3': "linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6)) ,url('./images/service_img3.png')",
           'learn': "linear-gradient(rgba(0, 0, 0, 0.001),rgba(0, 0, 0, 0.001)) ,url('./images/learn.jpg')",
+          'blog1': "linear-gradient(rgba(0, 0, 0, 0.60),rgba(0, 0, 0, 0.60)) ,url('./images/blog1.jpg')",
+          'blog2': "linear-gradient(rgba(0, 0, 0, 0.60),rgba(0, 0, 0, 0.60)) ,url('./images/blog2.png')",
+          'blog3': "linear-gradient(rgba(0, 0, 0, 0.60),rgba(0, 0, 0, 0.60)) ,url('./images/blog3.png')",
+          
         }
       }
     }
