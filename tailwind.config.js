4b0bac233022c8895a4f367f70fd90a05c4ca28b
sajjad-007 <sajjadhossain8123@gmail.com>
2023-12-23 13:31:30 +0600

    banner section complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,17 @@ tailwind.config = {
         colors: {
           clifford: '#da373d',
           headingbg: '#282828',
-          navbg: '#F40404',
+          primary: '#F40404',
         },
         spacing: {
             'container': '1144px',
         },
         fontFamily: {
             'poppins': "'Poppins', sans-serif",
-          }
+        },
+        backgroundImage: {
+          'bannerimg': "linear-gradient(rgba(0, 0, 0, 0.6),rgba(0, 0, 0, 0.6)) ,url('./images/banner_img.jpg')",
+        }
       }
     }
   }
